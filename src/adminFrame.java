65a0c1ea096f279c928bb5ac67e320e24fa1caa9
DeerSky <43960771@qq.com>
2022-05-23 17:44:39 +0800

    一个只有弹窗的管理员界面（悲）

---
+++ src/adminFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+
+public class adminFrame extends JFrame {
+    public  adminFrame() {
+        JOptionPane.showMessageDialog(null,"我还没写！","",JOptionPane.ERROR_MESSAGE);
+
+    }
+}
